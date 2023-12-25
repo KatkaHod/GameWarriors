43c03d19888f3c4bcc8239ac1d67e83e1ba04cce
KatkaHod <hodslavska.k@gmail.com>
2023-12-25 22:00:16 +0100

    A new constructor added

--- TurnBasedFight/Cube.java
+++ TurnBasedFight/Cube.java
@@ -1,32 +1,34 @@
 package TurnBasedFight;
+
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * the class represents the playing cube
  * The class has two attributes, private modifier - visible in class only
  * Always set private modifier, except when I need public (some cases)
+ * Constructor- the method that is called when the object instance is created.
+ * *
+ * Adding constructor to class - public Cube
+ *      * No return type!
+ *      * Declare same way as method
+ *      * Name must be same as a name of Class!
  */
 
 public class Cube {
 
-   private int numberOfWalls;
+   private static int CountOfWalls; //New variable represents Count of walls in cube, cannot be modified externally
+   private Random random; //New variable represents random number generator
 
-   /**
-   Data type is "Random" a new private atribut random
-    * Represents random number generator
-    */
-   private Random random;
-
-
-    /** Creation of new constructor
-     * No return type!
-     * Declare same way as method
-     * Name must be same as a name of Class!
-     */
+    //Creating a new instance of cube
    public Cube() {
-       numberOfWalls = 6; //Setting up the six walls
+       CountOfWalls = 6; //Setting up the six walls
        random = new Random(); //instance of Random class
-
    }
+
+   //Return count of walls, READ-ONLY, User can see only the value inside, can not change!
+    public static int ReturnCountOfWalls() {
+        return CountOfWalls;
+    }
 
 } //End of the public class Cube
---
+++ TurnBasedFight/Fight.java
@@ -0,0 +1,13 @@
+package TurnBasedFight;
+
+public class Fight {
+
+    public static void main(String[] args) {
+
+        Cube cube = new Cube(); //New instance of Cube class, call the constructor
+        System.out.println(Cube.ReturnCountOfWalls());
+
+
+
+    }
+}
