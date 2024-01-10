67027d3c7c0e8b339ccf990dd33987286b1c7cdf
KatkaHod <hodslavska.k@gmail.com>
2024-01-10 22:14:13 +0100

    .

--- TurnBasedFight/Fighter.java
+++ TurnBasedFight/Fighter.java
@@ -64,12 +64,37 @@ public class Fighter {
      * Fighter is live if he has more hp than 0
      */
 
-    public boolean fighterAlive() {
+    public boolean isAlive() {
         return (life > 0);
     }
 
-    //continue with graphic life
+    /**
+     * Graphic representation of life
+     * @return
+     */
 
+    public String GraphicLife() {
+        String GraphicLife = "[";
+        int total = 20;
+
+        double countParts = Math.round(((double) life / maxLife) * total);
+
+        if ((countParts == 0)) && ((isAlive()) {
+            countParts = 1;
+        }
+        for (int i = 0; i < countParts; i++) {
+            GraphicLife += "#";
+        }
+        for (int i = 0; i < total - countParts; i++) {
+            GraphicLife += " ";
+        }
+        GraphicLife += "]";
+        return GraphicLife;
+    }
+
+
+
+    }
 
 
 
